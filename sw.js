71206800333bb96cecf8373b81d3d1d76df5c34c
bomberoscubarral <bomberoscubarral@gmail.com>
2026-06-06 18:01:44 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "directorio-emergencias-v2.4.2";
+const CACHE_NAME = "directorio-emergencias-v2.4.3";
 
 const urlsToCache = [
   "./",
@@ -24,7 +24,8 @@ const urlsToCache = [
 
   "./app.js",
 
-  
+  "https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap",
+  "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
 ];
 
 // INSTALACIÓN
